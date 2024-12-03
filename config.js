19cc79df3550d10c67372e2a9d6f28909ffed0e7
SLEEKFFX <166394789+SLEEKFFX@users.noreply.github.com>
2024-12-03 18:15:56 +0100

    config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const toBool = x => x === 'true';
 const DATABASE_URL = process.env.DATABASE_URL || './database.db';
 const config = {
 	BASE_API_URL: process.env.BASE_API_URL || 'https://xstro-api-ec3ad328e76f.herokuapp.com',
-	SESSION_ID: process.env.SESSION_ID || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_39_00_26',
 	BOT_INFO: process.env.BOT_INFO || 'ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ',
 	PREFIX: process.env.PREFIX || '.',
 	SUDO: process.env.SUDO || '',
